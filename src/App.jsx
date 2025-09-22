9ee01ebe3734e4afc6364fefe7db531dd9397e9a
bkapil002 <kapilabrahm@gmail.com>
2025-09-22 14:01:51 +0545

    email

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,19 @@
-import React, { useState } from "react";
 
-export default function RegisterForm() {
-  const [formData, setFormData] = useState({});
+function App() {
 
-  const handleSubmit = (e) => {
-    e.preventDefault(); // stop actual POST request
-
-    // Create FormData object from the form
-    const data = new FormData(e.target);
-
-    // Convert FormData to an object
-    const values = Object.fromEntries(data.entries());
-
-    // Save in state (just for printing here)
-    setFormData(values);
-
-    console.log("Form Values:", values);
-  };
 
   return (
-    <div style={{ maxWidth: "400px", margin: "auto" }}>
-      <h2>Register</h2>
-
-      <form method="POST" onSubmit={handleSubmit}>
-        <div>
-          <label>Name:</label>
-          <br />
-          <input type="text" name="name" required />
-        </div>
-
-        <div>
-          <label>Email:</label>
-          <br />
-          <input type="email" name="email" required />
-        </div>
-
-      
-
+    <>
+     <form method="get" action="https://samzra.onrender.com/signin">
+        <input 
+          id="fname" 
+          defaultValue="cHJlbUBicmFuZGluZ3JhaW4uY29t" 
+        />
         <br />
-        <button type="submit">Register</button>
+        <input type="submit" value="Submit" />
       </form>
-
-      {/* Print submitted values */}
-      {formData.name && (
-        <div
-          style={{
-            marginTop: "20px",
-            padding: "10px",
-            border: "1px solid gray",
-          }}
-        >
-          <h3>Submitted Values</h3>
-          <p>
-            <b>Name:</b> {formData.name}
-          </p>
-          <p>
-            <b>Email:</b> {formData.email}
-          </p>
-    
-        </div>
-      )}
-    </div>
-  );
+    </>
+  )
 }
+
+export default App
